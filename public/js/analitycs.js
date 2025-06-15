a6208998108065e9e2ecc49d7878fd69a6374168
Russel Quiam <russelquiam@hotmail.com>
2025-06-14 23:08:42 -0500

    incia version astro

--- public/js/analitycs.js
+++
@@ -1,7 +0,0 @@
-//Global site tag (gtag.js) - Google Analytics -->
-
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-146771936-1');
