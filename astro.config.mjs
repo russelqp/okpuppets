8d96f89dffc432d7a2a7d85a9bd4e59a6da36b32
Russel Quiam <russelquiam@hotmail.com>
2025-06-16 11:59:56 -0500

    se actualizo sitio con astro, falta optimizar carga de imagenes y seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.okpuppets.com',
+  site: 'https://www.okpuppets.com/',
+
   //base: '/docs',
   trailingSlash: "always",
 
@@ -15,12 +20,15 @@ export default defineConfig({
       inlineStylesheets: `auto`,
       format: 'file',
       //assets: '_custom'
-      //assetsPrefix: 'https://www.okpuppets.com/',
+      assetsPrefix: 'https://www.okpuppets.com/',
   },
+
   vite: {
     plugins: [tailwindcss()],
     css: {
       transformer: "lightningcss",
     },
   },
+
+  integrations: [sitemap(), robotsTxt()],
 });
